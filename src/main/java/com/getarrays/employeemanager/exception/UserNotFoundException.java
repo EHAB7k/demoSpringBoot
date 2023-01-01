258039e9339e75833112421f377c3319c95bb323
Ehab hakami <ehab7k@gmail.com>
2023-01-01 10:25:28 +0300

    update1.0.0

---
+++ src/main/java/com/getarrays/employeemanager/exception/UserNotFoundException.java
@@ -0,0 +1,6 @@
+package com.getarrays.employeemanager.exception;
+
+public class UserNotFoundException extends RuntimeException {
+    public UserNotFoundException(String message) {super(message);
+    }
+}
